c08a07059bc482d31e53b8b02c7dc00428cef983
Ubuntu <ubuntu@ip-172-31-40-13.ec2.internal>
2021-12-08 17:02:22 +0000

    added numGuess.cpp

---
+++ numGuess.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2021 Alexander Matheson All rights reserved.
+//
+// Created by: Alexander matheson
+// Created on: Dec 8 2021
+// This program gets the user to guess a number
+// between 0 and 9 and tells them if it is correct.
+#include <iostream>
+
+
+int main() {
+    // this function checks if ther is over 30 students
+    const int CORRECT = 8;
+    float userNumber;
+
+    // input
+    std::cout << "What is your number? ";
+    std::cin >> userNumber;
+    std::cout << "" << std::endl;
+
+    // process
+    if (userNumber == CORRECT) {
+        // output
+        std::cout << "Correct!";
+    } else if (userNumber >= 10) {
+        // output
+        std::cout << "Number must be between 0 and 9.";
+    } else if (userNumber <= -1) {
+        // output
+        std::cout << "Number must be between 0 and 9.";
+    }  else {
+        // output
+        std::cout << "Wrong. :-(";
+    }
+}
